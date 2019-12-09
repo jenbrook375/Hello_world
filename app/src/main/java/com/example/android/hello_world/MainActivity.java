db6dcbb83d800c182e5f83e90e82efb53987d15c
JenBrook <jenbrook375@hotmail.com>
2019-12-09 13:45:10 -0800

    added setText and increment button

--- app/src/main/java/com/example/android/hello_world/MainActivity.java
+++ app/src/main/java/com/example/android/hello_world/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.android.hello_world;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // textView and button ids
+    TextView tvCounter;
+    Button btnIncrement;
+
+    // create variable of type integer and set it to 0
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find these id's in their views
+        tvCounter = findViewById(R.id.tvCounter);
+        btnIncrement = findViewById(R.id.btnIncrement);
+
+        tvCounter.setText("0");
+
+        btnIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter = counter +1;
+                tvCounter.setText(String.valueOf(counter));
+            }
+        });
     }
 }
